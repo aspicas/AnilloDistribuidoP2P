10cc3474f2700d727cef318b89358dbd7c713608
David Garcia <david.a.g.r93@gmail.com>
2017-12-29 00:24:55 -0400

    Creada las clases Registry, Cliente y Main

---
+++ Node/src/global/Registry.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package global;
+
+/**
+ *
+ * @author david
+ */
+public class Registry {
+    public static String ghost = "192.168.1.1";
+    public static String predecessor = "";
+    public static String sucessor = "";
+    public static int clientPort = 2001;
+    public static int serverPort = 2000;
+    public static String ip = "192.168.1.1";
+}
---
+++ Node/src/model/Cliente.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import global.Registry;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author david
+ */
+public class Cliente {
+    private Socket client = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+
+    public Cliente() {
+        try {
+            this.client = new Socket(Registry.ip, Registry.clientPort);
+            this.input = new DataInputStream(client.getInputStream());
+            this.output = new DataOutputStream(client.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }  catch (Exception ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void searchResource(){
+        
+    }
+    
+    public void requestStatus(){
+        
+    }
+    
+    public void defineRing(){
+        
+    }
+    
+    public void disconnet(){
+        
+    }
+}
