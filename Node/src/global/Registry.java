bdc8b91c05a0d2fb28f1732aa043565d802c3b68
David Garcia <david.a.g.r93@gmail.com>
2018-01-08 23:47:47 -0400

    Restriccion de sucessor y predecesor

--- Node/src/global/Registry.java
+++ Node/src/global/Registry.java
@@ -18,7 +18,7 @@ import view.ResourceView;
  */
 public class Registry {
     //Communication
-    public static final String ghost = "192.168.1.101";
+    public static final String ghost = "192.168.1.100";
     public static final int port = 2000;
     public static final String address = "192.168.1.112";
     
